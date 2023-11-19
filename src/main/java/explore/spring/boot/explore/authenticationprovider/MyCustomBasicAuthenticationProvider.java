fe452dc1b7ec601648510ec5f45b02445bd321cc
Dhananjaya Rao G <meet.dhanuhyd@gmail.com>
2023-11-19 11:18:41 +0530

    Added getEmployees(Authentication authentication) which is available after initial spring process to contorller

---
+++ src/main/java/explore/spring/boot/explore/authenticationprovider/MyCustomBasicAuthenticationProvider.java
@@ -0,0 +1,6 @@
+package explore.spring.boot.explore.authenticationprovider;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+
+public class MyCustomBasicAuthenticationFilter implements AuthenticationProvider {
+}
